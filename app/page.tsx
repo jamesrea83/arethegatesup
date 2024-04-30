0a07245babe6373113633ed66f52fddcddc5b6c5
jamesrea83 <james@stream.bid>
2024-04-30 09:24:12 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,82 @@
-import Image from "next/image";
+import Image from 'next/image';
+const soapRequest = require('easy-soap-request');
+var parseString = require('xml2js').parseString;
+const { XMLParser, XMLBuilder, XMLValidator } = require('fast-xml-parser');
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const xmlBody = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2016-02-16/ldb/">
+    <soap:Header>
+        <typ:AccessToken>
+            <typ:TokenValue>${process.env.ACCESS_TOKEN}</typ:TokenValue>
+        </typ:AccessToken>
+    </soap:Header>
+    <soap:Body>
+        <ldb:GetArrBoardWithDetailsRequest>
+            <ldb:numRows>10</ldb:numRows>
+            <ldb:crs>HMD</ldb:crs>
+            <ldb:filterCrs></ldb:filterCrs>
+            <ldb:filterType>to</ldb:filterType>
+            <ldb:timeOffset>0</ldb:timeOffset>
+            <ldb:timeWindow>120</ldb:timeWindow>
+        </ldb:GetArrBoardWithDetailsRequest>
+    </soap:Body>
+</soap:Envelope>`;
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+export default async function Home() {
+	const url = 'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb9.asmx';
+	const sampleHeaders = {
+		'Content-Type': 'text/xml',
+		Accept: '*/*',
+	};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const { response } = await soapRequest({
+		url: url,
+		headers: sampleHeaders,
+		xml: xmlBody,
+		timeout: 10000,
+	}); // Optional timeout parameter(milliseconds)
+	const { headers, body, statusCode } = response;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const parser = new XMLParser();
+	let jObj = parser.parse(body);
+	const data =
+		jObj['soap:Envelope']['soap:Body'].GetArrBoardWithDetailsResponse
+			.GetStationBoardResult['lt5:trainServices']['lt5:service'];
+	console.log(data);
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+	// const parsed = await parseString(body, (err, result) => {
+	// 	console.log(result['soap:Body']);
+	// });
+	// console.log(parsed);
+	// const response = await fetch(
+	// 	'https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb9.asmx',
+	// 	{
+	// 		method: 'POST',
+	// 		headers: {
+	// 			'Content-Type': 'text/xml',
+	// 			Accept: '*/*',
+	// 		},
+	// 		body: 'xmlBody',
+	// 		cache: 'no-cache',
+	// 	}
+	// );
+	// const data = await response.toString();
+	// console.log(response);
+	// const xmlDoc = parser.parseFromString(text, 'text/xml');
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
+			{/* {JSON.stringify(data)} */}
+			{data.map(item => {
+				const keys = Object.keys(item);
+				return (
+					<div className='my-4'>
+						{keys.map(key => (
+							<div>
+								{key} - {item[key].toString()}
+							</div>
+						))}
+					</div>
+				);
+			})}
+		</main>
+	);
 }
