0b7a6cd844ccbc604108c117f99fd4b54a1dc183
jamesrea83 <james@stream.bid>
2024-05-02 13:52:13 +0100

    loggin

--- utils/getMinsTilEstimate.ts
+++ utils/getMinsTilEstimate.ts
@@ -1,14 +1,17 @@
 import getTimeStampFromString from '@/utils/getTimeStampFromString';
 
 const getMinsTilEstimate = (generatedAt: string, estimateDateObject: Date) => {
-	const nowDateObject = getTimeStampFromString(generatedAt);
-	const msDifference = estimateDateObject.getTime() - nowDateObject.getTime();
+	const generatedAtDateObject = getTimeStampFromString(generatedAt);
+	const msDifference =
+		estimateDateObject.getTime() - generatedAtDateObject.getTime();
 	const result = Math.round(msDifference / (1000 * 60));
-	// console.log('getMinsTilEstimate', msDifference, result);
-	// console.log('***');
-	// console.log('getMinsTilEstimate estimateDateObject', estimateDateObject);
-	// console.log('getMinsTilEstimate nowDateObject', nowDateObject);
-	// console.log('***');
+	console.log('*** getMinsTilEstimate');
+	console.log('generatedAt', generatedAt);
+	console.log('generatedAtDateObject', generatedAtDateObject);
+	console.log('estimateDateObject', estimateDateObject);
+	console.log('msDifference', msDifference);
+	console.log('result', result);
+	console.log('*** /getMinsTilEstimate');
 	return result;
 };
 
